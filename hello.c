42456578fd6cc2f85a8a2a1987d384b19de627e4
Jaehyuk Hur <gjwogur71@naver.com>
2016-10-06 01:25:38 -0700

    test

--- hello.c
+++ hello.c
@@ -2,12 +2,9 @@
 
 int main()
 {
-	printf("Hello!!!!!!!dgfg#$!!!!!1!\n");
-	printf("HaHadddd$%^gddd\n");
-	printf("ddddvdvdver"\n);
-	pinrtf("asdfddfff\""\n);
 	printf("Hello!!!@#$%%^$#@^&!!!!!!!!!1!\n");
 	printf("Haddddddd\n");
 	printf("ddddvdv");
+	printf("\n\n\n\n\n");
 	return 0;
 }
